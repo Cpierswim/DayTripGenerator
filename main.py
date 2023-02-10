4e8526456672ce2a4a276d7068aa291cd1c1dc68
Chris Pierson <cpierswim@gmail.com>
2023-02-10 14:45:21 -0500

    Added main functionality to select trip and repick if it's not what is desired

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import random
 
+#Return an item from a supplied list at random
 def select_random_from_list(list):
-    return list[random(0, len(list) - 1)]
+    return list[random.randint(0, len(list) - 1)]
 
+#Returns a list of predefined desinationa
 def get_destinations_list():
     list = []
     list.append("Perth, WA")
@@ -16,6 +18,7 @@ def get_destinations_list():
     list.append("San Franciso, CA")
     return list
 
+#Returns a list of predefined restaurants
 def get_restaurants_list():
     list = []
     list.append("Gino's  Pizza")
@@ -28,6 +31,7 @@ def get_restaurants_list():
     list.append("Okiboru Tsukemen & Ramen")
     return list
 
+#Returns a list of predefined transportation modes
 def get_transportation_modes_list():
     list = []
     list.append("Car")
@@ -40,6 +44,7 @@ def get_transportation_modes_list():
     list.append("Rickshaw")
     return list
 
+#Returns a list of predefined types of entertainment 
 def get_entertainment_list():
     list = []
     list.append("Magic Show")
@@ -47,10 +52,28 @@ def get_entertainment_list():
     list.append("Live Show")
     list.append("Concert")
     list.append("Football Game")
+    return list
 
+def print_selections(destination, restaurant, transportation_mode, entertainment):
+    print(f"Desitnation: {destination}")
+    print(f"Restaurant: {restaurant}")
+    print(f"Entertainment: {entertainment}")
+    print(f"Transportation: {transportation_mode}")
 
+#Begin main program execution
 destinations = get_destinations_list()
 restaurants = get_restaurants_list()
 transporation_modes = get_transportation_modes_list()
 entertainments = get_entertainment_list()
 
+satisfied_with_selections = False
+while(not satisfied_with_selections):
+    destination = select_random_from_list(destinations)
+    restaurant = select_random_from_list(restaurants)
+    transportation_mode = select_random_from_list(transporation_modes)
+    entertainment = select_random_from_list(entertainments)
+
+    print_selections(destination, restaurant, transportation_mode, entertainment)
+    selection = input("Are you satisfied with your trip? Y or N: ") #Assuming nother other than Y or N will be entered
+    if selection == "Y":
+        satisfied_with_selections = True
